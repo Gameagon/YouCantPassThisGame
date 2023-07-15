4669189ec31dc155dbf6a01baccd722f532f4d94
sergisantos658 <80329554+sergisantos658@users.noreply.github.com>
2023-07-15 02:01:19 +0200

    Distincion mando y raton del new input system

--- Assets/Scripts/Player/Camera/FirstPersonCameraController.cs
+++ Assets/Scripts/Player/Camera/FirstPersonCameraController.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using UnityEngine.InputSystem.XInput;
 
 public class FirstPersonCameraController : MonoBehaviour
 {
     [SerializeField] Vector2 lookAngle;
     [SerializeField] Transform head;
-
+    [SerializeField] private float sensivilitygamepad = 30; 
 
     [SerializeField] float sensitivity = 2.0f;
     // Start is called before the first frame update.0
@@ -19,7 +20,19 @@ public class FirstPersonCameraController : MonoBehaviour
 
     public void Look(InputAction.CallbackContext context)
     {
-        lookAngle += context.ReadValue<Vector2>() * sensitivity;
+        Debug.Log(context.control.device);
+        if(context.control.device is Mouse)
+        {
+            lookAngle += context.ReadValue<Vector2>() * sensitivity;
+
+        }
+        else
+        {
+            lookAngle += context.ReadValue<Vector2>() * sensitivity * sensivilitygamepad;
+
+
+
+        }
         lookAngle.y = Mathf.Clamp(lookAngle.y, -90, 90);
         lookAngle.x = lookAngle.x % 360;
     }
